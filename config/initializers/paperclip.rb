c36c814ed1906603486bee14f1ff9bf214e07fbc
Nickolay Yegorov <nickolay@yegorov.info>
2014-08-08 15:52:44 +0400

    x

--- config/initializers/paperclip.rb
+++
@@ -1,4 +0,0 @@
-# config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:url] = 'http://quizzesasdasd.s3-website-eu-west-1.amazonaws.coml'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 'quizzesasdasd.s3-website-eu-west-1.amazonaws.com'
